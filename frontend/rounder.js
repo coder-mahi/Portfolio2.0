f04b20d20a1238040a0d2c349e0ac13fbc08de27
MAHESH <maheshshinde9100@gmail.com>
2024-12-29 23:06:59 +0530

    Major changes

--- frontend/rounder.js
+++ frontend/rounder.js
@@ -36,7 +36,7 @@ TxtType.prototype.tick = function() {
     setTimeout(function() {
     that.tick();
     }, delta);
-};
+    };
 
 window.onload = function() {
     var elements = document.getElementsByClassName('typewrite');
@@ -47,7 +47,6 @@ window.onload = function() {
           new TxtType(elements[i], JSON.parse(toRotate), period);
         }
     }
-    // INJECT CSS
     var css = document.createElement("style");
     css.type = "text/css";
     css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
